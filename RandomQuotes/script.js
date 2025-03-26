f24d4d27587dd315d8a7c123f09a1963c4ead4f2
Thomas O'Connor <thomas.Ocono@gmail.com>
2025-03-26 13:26:15 -0400

    Random quote maker done, even got twitter thing working

--- RandomQuotes/script.js
+++ RandomQuotes/script.js
@@ -1,23 +1,58 @@
+const text = document.getElementById("text");
+const author = document.getElementById("author");
+const tweet = document.getElementById("tweet-quote");
+const newQuote = document.getElementById("new-quote");
 const quotes = [
-  "Be yourself; everyone else is already taken.",
-  "So many books, so little time.",
-  "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
-  "A room without books is like a body without a soul",
-  "You only live once, but if you do it right, once is enough.",
-];
-const authors = [
-  "Oscar Wilde",
-  "Frank Zappa",
-  "Albert Einstein",
-  "Marcus Tullius Cicero",
-  "Mae West",
+  [
+    "Life isn't about getting and having, it's about giving and being.",
+    "Kevin Kruse",
+  ],
+  [
+    "Whatever the mind of man can conceive and believe, it can achieve.",
+    "Napoleon Hill",
+  ],
+  ["Strive not to be a success, but rather to be of value.", "Albert Einstein"],
+  [
+    "Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.",
+    "Robert Frost",
+  ],
+  [
+    "I attribute my success to this: I never gave or took any excuse.",
+    "Florence Nightinggale",
+  ],
 ];
 
-const quoteText = document.getElementById("text");
-const authorText = document.getElementById("author");
-const quoteButton = document.getElementById("new-quote");
+randomQuote = Math.floor(Math.random() * quotes.length);
+console.log(randomQuote);
 
-function getRandom() {
-  return Math.floor(Math.random() * quotes.length);
+function updateQuote(rando) {
+  text.innerText = quotes[rando][0];
+  author.innerText = quotes[rando][1];
 }
-let random = getRandom();
+function convertToTwitter(random) {
+  quote = quotes[random][0];
+  twitterQuote = quote.replace(/\s/g, "%20");
+  console.log(twitterQuote);
+  return twitterQuote;
+}
+tweet.href =
+  "https://twitter.com/intent/tweet?text=" +
+  convertToTwitter(randomQuote) +
+  " - " +
+  quotes[randomQuote][1];
+
+updateQuote(randomQuote);
+convertToTwitter(randomQuote);
+
+newQuote.addEventListener("click", () => {
+  prevRandom = randomQuote;
+  while (randomQuote == prevRandom) {
+    randomQuote = Math.floor(Math.random() * quotes.length);
+  }
+  updateQuote(randomQuote);
+  tweet.href =
+    "https://twitter.com/intent/tweet?text=" +
+    convertToTwitter(randomQuote) +
+    " - " +
+    quotes[randomQuote][1];
+});
